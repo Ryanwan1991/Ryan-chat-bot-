9a8eeaac3b94af6fb9b54111c6e75cf3ed920a8e
RyanWan <50205532+Ryanwan1991@users.noreply.github.com>
2020-12-04 16:52:14 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -10,21 +10,17 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(
-    'f1UkFaQj7dU1qoOdANntXBlmN/yd1+HssohiZXufOscLa7FAT9Wa3p2Tes92lb8s39/cG+htP/dyY2ApYYijsO7KwWb/Kh02yXn706pJstsmtTUtpuV0tYKtYXiR/XOIj9fC+VYKmIvOA8T4rctmTwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('3c0582d8762771c056e4339a3c759ce5')
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
 @app.route("/")
 def home():
-    return 'Test is OK'
+    return 'home OK'
 
 # 監聽所有來自 /callback 的 Post Request
-
-
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -47,17 +43,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if 'hi' in event.message.text:
-        line_bot_api.reply_message(
-            event.reply_token,  # 回覆後即丟棄此token
-            TextSendMessage(text='今天天氣真好'))
-
-
-'''
     line_bot_api.reply_message(
-        event.reply_token,  # 回覆後即丟棄此token
+        event.reply_token,
         TextSendMessage(text=event.message.text))
-'''
+
 
 if __name__ == "__main__":
     app.run()
+    
